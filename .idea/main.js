997a6909664c2eb5e921fd90f46a6cc12fef47a3
Elnura <elnura@Elnuras-MacBook-Pro.local>
2024-12-20 23:58:37 +0600

    Medium version

--- .idea/main.js
+++ .idea/main.js
@@ -1,7 +1,10 @@
 let countdown_timer;
-let remaining_time = 2;
+let remaining_work_time = 25 * 60;
+let remaining_break_time = 2 * 60;
+let previous_work_interval;
+let previous_break_interval;
 let completed_sessions = 0;
-let previous_interval = 2;
+let session_type = "work";
 
 var countdown = document.getElementById("countdown");
 const start_text = document.getElementsByClassName("start-text")[0];
@@ -11,23 +14,23 @@ const secret_images = document.getElementsByClassName("secret-images");
 const good_job_text = document.getElementsByClassName("good-job-text");
 var audio = document.getElementById('myAudio');
 const sessions_text = document.getElementsByClassName("sessions-text")[0];
+const session_type_texts = document.getElementsByClassName("session-type-text");
+const line1 = document.getElementById("line1");
+const line2 = document.getElementById("line2");
 
 audio.addEventListener("ended", function () {
     hideImagesAndText();
+    startBreakSession();
 });
 
 function start_timer() {
     audio.pause();
     audio.currentTime = 0;
     countdown.disabled = true;
-    start_text.textContent = "Time to work :)";
     start_button.style.display = "none";
-    restart_button.style.display = "block";
     hideImagesAndText();
-
     try {
         const time = countdown.value.split(":");
-
         if (time.length !== 2) {
             throw new Error("Invalid time format. Please use the format mm:ss.");
         }
@@ -38,32 +41,61 @@ function start_timer() {
             throw new Error("Invalid time format. Please use the format mm:ss.");
         }
 
-        remaining_time = minutes * 60 + seconds;
-        previous_interval = remaining_time;
+        if (session_type === "work") {
+            start_text.textContent = "Time to work :)";
+            restart_button.style.display = "block";
+            remaining_work_time = minutes * 60 + seconds;
+            previous_work_interval = remaining_work_time;
+            highlightWorkSession();
+        } else {
+            remaining_break_time = minutes * 60 + seconds;
+            previous_break_interval = remaining_break_time;
+            highlightBreakSession();
+        }
         countdown_timer = setInterval(updateCountdown, 1000);
 
     } catch (error) {
         alert("Error: " + error.message);
-        resetElements();
+        resetElementsWorkSession();
         hideImagesAndText();
     }
 }
 
 function updateCountdown() {
-    if (remaining_time <= 0) {
-        clearInterval(countdown_timer);
-        completed_sessions++;
-        audio.play();
-        resetElements();
-        showImagesAndText();
-        return;
-    } else {
-        var minutes = Math.floor(remaining_time / 60);
-        var seconds = remaining_time % 60;
-        var minutesString = formatTime(minutes);
-        var secondsString = formatTime(seconds);
-        countdown.value = minutesString + ":" + secondsString;
-        remaining_time--;
+    if(session_type === "work") {
+
+        if (remaining_work_time <= 0) {
+            session_type = "break";
+            highlightBreakSession();
+            clearInterval(countdown_timer);
+            resetElementsBreakSession();
+            completed_sessions++;
+            audio.play();
+            showImagesAndText();
+            return;
+        } else {
+            var minutes = Math.floor(remaining_work_time / 60);
+            var seconds = remaining_work_time % 60;
+            var minutesString = formatTime(minutes);
+            var secondsString = formatTime(seconds);
+            countdown.value = minutesString + ":" + secondsString;
+            remaining_work_time--;
+        }
+    }else{
+        if (remaining_break_time <= 0) {
+            session_type = "work";
+            highlightWorkSession();
+            clearInterval(countdown_timer);
+            resetElementsWorkSession();
+            return;
+        } else {
+            var minutes = Math.floor(remaining_break_time / 60);
+            var seconds = remaining_break_time % 60;
+            var minutesString = formatTime(minutes);
+            var secondsString = formatTime(seconds);
+            countdown.value = minutesString + ":" + secondsString;
+            remaining_break_time--;
+        }
     }
 }
 
@@ -75,14 +107,16 @@ function formatTime(time) {
 }
 
 function restart_timer() {
-    clearInterval(countdown_timer);
-    remaining_time = previous_interval;
-    var minutes = Math.floor(remaining_time / 60);
-    var seconds = remaining_time % 60;
-    var minutesString = formatTime(minutes);
-    var secondsString = formatTime(seconds);
-    countdown.value = minutesString + ":" + secondsString;
-    countdown_timer = setInterval(updateCountdown, 1000);
+    if (session_type === "work") {
+        clearInterval(countdown_timer);
+        remaining_work_time = previous_work_interval;
+        var minutes = Math.floor(remaining_work_time / 60);
+        var seconds = remaining_work_time % 60;
+        var minutesString = formatTime(minutes);
+        var secondsString = formatTime(seconds);
+        countdown.value = minutesString + ":" + secondsString;
+        countdown_timer = setInterval(updateCountdown, 1000);
+    }
 }
 
 countdown.addEventListener('keydown', function(event) {
@@ -99,13 +133,26 @@ countdown.addEventListener('keydown', function(event) {
     }
 });
 
-function resetElements() {
+function resetElementsWorkSession() {
     countdown.disabled = false;
-    start_text.textContent = "Start countdown";
     sessions_text.textContent = "You have completed " + completed_sessions + " sessions";
     start_button.style.display = "block";
     restart_button.style.display = "none";
-    countdown.value = "25:00";
+    var minutes = Math.floor(remaining_work_time / 60);
+    var seconds = remaining_work_time % 60;
+    var minutesString = formatTime(minutes);
+    var secondsString = formatTime(seconds);
+    countdown.value = minutesString + ":" + secondsString;
+}
+
+function resetElementsBreakSession(){
+    countdown.disabled = false;
+    restart_button.style.display = "none";
+    var minutes = Math.floor(remaining_break_time / 60);
+    var seconds = remaining_break_time % 60;
+    var minutesString = formatTime(minutes);
+    var secondsString = formatTime(seconds);
+    countdown.value = minutesString + ":" + secondsString;
 }
 
 function showImagesAndText() {
@@ -122,4 +169,25 @@ function hideImagesAndText() {
     secret_images[1].currentTime = 0;
     good_job_text[0].style.display = "none";
     good_job_text[1].style.display = "none";
+}
+
+function highlightWorkSession() {
+    session_type_texts[0].style.color = "rgb(46, 46, 46)";
+    line1.style.backgroundColor = "rgb(46, 46, 46)";
+    session_type_texts[1].style.color = "rgb(166, 166, 166)";
+    line2.style.backgroundColor = "rgb(166, 166, 166)";
+    start_text.textContent = "Start countdown";
+}
+
+function highlightBreakSession() {
+    session_type_texts[1].style.color = "rgb(46, 46, 46)";
+    line2.style.backgroundColor = "rgb(46, 46, 46)";
+    session_type_texts[0].style.color = "rgb(166, 166, 166)";
+    line1.style.backgroundColor = "rgb(166, 166, 166)";
+    start_text.textContent = "Take a rest";
+}
+
+function startBreakSession(){
+    start_button.style.display = "block";
+    start_timer();
 }
